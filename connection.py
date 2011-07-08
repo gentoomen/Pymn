312436ff1172c794fe4a56d823ffbc8fdce550c9
Sean Jagat <thewhiteneighbour@gmail.com>
2011-07-08 14:44:09 -0700

    minor tweaks to connection.py

--- connection.py
+++ connection.py
@@ -4,8 +4,9 @@
 import Queue
 import socket
 import time
+import threading
 
-class Connection:
+class Connection (threading.Thread):
 
   __connection = None
   __messages = None
@@ -20,28 +21,36 @@ class Connection:
     self.__host = host
     self.__port = port
     self.connect()
+    threading.Thread.__init__(self)
     
   def connect(self):
+    print "connecting"
     self.__connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     self.__connection.connect((self.__host, self.__port))
-    self._messages = Queue()
+    self.__messages = Queue.LifoQueue()
     self.__connected = True
     
   def sendMessage(self, msg):
+    print "sending message"
     self.__connection.send(msg)
       
   def getMessage(self):
+    print "getting message"
     return self.__messages.get(True, self.__timeout)
   
   def hasMessage(self):
+    print "checking messages"
     return not self.__messages.empty()
       
   def run (self):
     while (self.__connected):
       message = self.__connection.recv(self.__bufferSize)
+      print message
       while (not message == ''):
-        partition = str.partition(message)
+        partition = ''
+        partition = message.partition(partition, "\n")
         self.__messages.put(partition[0], True, self.__timeout)
         message = partition[2]
+      print "sleeping"
       time.sleep(self.__sleepTime)
 
