c699f2b3691480aef62d05265929dbcf17f955d1
Sean Jagat <thewhiteneighbour@gmail.com>
2011-07-16 02:44:31 -0700

    Proper line separation in connection.py

---
+++ testconfig.py
@@ -0,0 +1,10 @@
+#using this for test values for config
+
+host = "irc.rizon.net"
+port = 6667
+
+username = testUserName8462
+hostname = testHostName8462
+servername = testServerName8462
+realname = testRealName8462
+
