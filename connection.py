6a48671b3af83c80e9c60dec42747fc1434fd511
Sean Jagat <thewhiteneighbour@gmail.com>
2011-07-16 03:35:38 -0700

    small fix regarding printing buffered  information, added comments

--- connection.py
+++ connection.py
@@ -4,16 +4,16 @@ import time
 
 class Connection (threading.Thread):
 
-  host = None
-  port = None
-  sock = None
+  host = None #server hostname
+  port = None #server port
+  sock = None #socket object to connect to server
 
   username = None
   hostname = None
   servername = None
   realname = None
 
-  buffer = None
+  buffer = None #buffer used to store incomplete lines
 
   def __init__(self, host, port, username, hostname, servername, realname):
     self.host = host
@@ -26,22 +26,34 @@ class Connection (threading.Thread):
 
     self.buffer = ''
   
+    #initialize connection
     self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     self.sock.connect((host, port))
 
+    #start new thread for connection
     threading.Thread.__init__(self)
 
+  #helper function to send raw data over socket
   def send(self, msg):
     self.sock.send(msg + "\r\n")
 
+  #infinite loop:
+  #1. grab 4kb of data
+  #2. split data into lines
+  #3. if there is data in the buffer, prepend it to the first line
+  #4. if there is an incomplete line, store it in the buffer
+  #5. print all lines (later will parse all lines)
+  #6. sleep for a reasonable amount of time
   def run(self):
     while True:
       data = self.sock.recv(4096) 
       if (data != ''):
         lines = data.splitlines()
-        if (data[-1] != '\n'):
+        if (self.buffer != ''):
           lines[0] = self.buffer + lines[0]
-          buffer = lines.pop(-1)
+          self.buffer = ''
+        if (data[-1] != '\n'):
+          self.buffer = lines.pop(-1)
         for line in lines:
           print "<- " + line
       time.sleep (0.1)
